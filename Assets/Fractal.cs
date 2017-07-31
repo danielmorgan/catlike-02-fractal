92e795b0a2ed188154674bdc0243eaf623d2f8ea
Daniel Morgan <me@danielmorgan.co.uk>
2017-07-31 21:57:46 +0000

    Add more randomness to the fractal

--- Assets/Fractal.cs
+++ Assets/Fractal.cs
@@ -8,27 +8,38 @@ public class Fractal : MonoBehaviour {
     public float childScale;
     public int maxDepth;
     private int depth;
-    private static Vector3[] childDirections = {
-        Vector3.up,
-        Vector3.right,
-        Vector3.left,
-        Vector3.forward,
-        Vector3.back
-    };
-    private static Quaternion[] childOrientations = {
-        Quaternion.identity,
-        Quaternion.Euler(0f, 0f, -90f),
-        Quaternion.Euler(0f, 0f, 90f),
-        Quaternion.Euler(90f, 0f, 0f),
-        Quaternion.Euler(-90f, 0f, 0f),
-    };
+    private static Vector3[] childDirections = { Vector3.up, Vector3.right, Vector3.left, Vector3.forward, Vector3.back };
+    private static Quaternion[] childOrientations = { Quaternion.identity, Quaternion.Euler(0f, 0f, -90f), Quaternion.Euler(0f, 0f, 90f), Quaternion.Euler(90f, 0f, 0f), Quaternion.Euler(-90f, 0f, 0f), };
+    private Material[,] materials;
+    public Mesh[] meshes;
+    public float spawnProbability;
+    public float maxRotationSpeed;
+    private float rotationSpeed;
+    public float maxTwist;
+
+    private void InitializeMaterials () {
+        materials = new Material[maxDepth + 1, 2];
+
+        for (int i = 0; i <= maxDepth; i++) {
+            float t = i / (maxDepth - 1f);
+            t *= t;
+            materials[i, 0] = new Material(this.material);
+            materials[i, 0].color = Color.Lerp(Color.white, Color.yellow, (float) t / 2);
+            materials[i, 1] = new Material(this.material);
+            materials[i, 1].color = Color.Lerp(Color.white, Color.cyan, (float) t / 2);
+        }
+    }
 
     private void Start () {
-        gameObject.AddComponent<MeshFilter>().mesh = mesh;
-        gameObject.AddComponent<MeshRenderer>().material = material;
+        rotationSpeed = Random.Range(-maxRotationSpeed, maxRotationSpeed);
+        transform.Rotate(Random.Range(-maxTwist, maxTwist), 0f, 0f);
 
-        Color color = Color.Lerp(Color.white, Color.yellow, (float)depth / maxDepth);
-        GetComponent<MeshRenderer>().material.color = color;
+        if (materials == null) {
+            InitializeMaterials();
+        }
+
+        gameObject.AddComponent<MeshFilter>().mesh = meshes[Random.Range(0, meshes.Length)];
+        gameObject.AddComponent<MeshRenderer>().material = materials[depth, Random.Range(0, 2)];
 
         if (depth < maxDepth) {
             StartCoroutine(CreateChildren());
@@ -37,22 +48,31 @@ public class Fractal : MonoBehaviour {
 
     private IEnumerator CreateChildren () {
         for (int i = 0; i < childDirections.Length; i++) {
-            yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
-            new GameObject("Fractal Child")
-                .AddComponent<Fractal>()
-                .Initialize(this, i);
+            if (Random.value < spawnProbability) {
+                yield return new WaitForSeconds(Random.Range(0.05f, 0.15f));
+                new GameObject("Fractal Child")
+                    .AddComponent<Fractal>()
+                    .Initialize(this, i);
+            }
         }
     }
 
     private void Initialize (Fractal parent, int childIndex) {
-        mesh = parent.mesh;
-        material = parent.material;
+        meshes = parent.meshes;
+        materials = parent.materials;
+        spawnProbability = parent.spawnProbability;
         maxDepth = parent.maxDepth;
+        maxRotationSpeed = parent.maxRotationSpeed;
+        maxTwist = parent.maxTwist;
         depth = parent.depth + 1;
         childScale = parent.childScale;
         transform.parent = parent.transform;
         transform.localScale = Vector3.one * childScale;
         transform.localPosition = childDirections[childIndex] * (0.5f + 0.5f * childScale);
         transform.localRotation = childOrientations[childIndex];
+    }
+
+    private void Update () {
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
     }
 }
