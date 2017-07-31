5826720ab702676cedf7cd50d582c4589e82cc8d
Daniel Morgan <me@danielmorgan.co.uk>
2017-07-31 20:55:11 +0000

    Refactor and change colour of materials

--- Assets/Fractal.cs
+++ Assets/Fractal.cs
@@ -5,22 +5,46 @@ public class Fractal : MonoBehaviour {
 
     public Mesh mesh;
     public Material material;
-
+    public float childScale;
     public int maxDepth;
     private int depth;
-
-    public float childScale;
+    private static Vector3[] childDirections = {
+        Vector3.up,
+        Vector3.right,
+        Vector3.left,
+        Vector3.forward,
+        Vector3.back
+    };
+    private static Quaternion[] childOrientations = {
+        Quaternion.identity,
+        Quaternion.Euler(0f, 0f, -90f),
+        Quaternion.Euler(0f, 0f, 90f),
+        Quaternion.Euler(90f, 0f, 0f),
+        Quaternion.Euler(-90f, 0f, 0f),
+    };
 
     private void Start () {
         gameObject.AddComponent<MeshFilter>().mesh = mesh;
         gameObject.AddComponent<MeshRenderer>().material = material;
+
+        Color color = Color.Lerp(Color.white, Color.yellow, (float)depth / maxDepth);
+        GetComponent<MeshRenderer>().material.color = color;
 
         if (depth < maxDepth) {
             StartCoroutine(CreateChildren());
         }
     }
 
-    private void Initialize (Fractal parent, Vector3 direction) {
+    private IEnumerator CreateChildren () {
+        for (int i = 0; i < childDirections.Length; i++) {
+            yield return new WaitForSeconds(Random.Range(0.1f, 0.5f));
+            new GameObject("Fractal Child")
+                .AddComponent<Fractal>()
+                .Initialize(this, i);
+        }
+    }
+
+    private void Initialize (Fractal parent, int childIndex) {
         mesh = parent.mesh;
         material = parent.material;
         maxDepth = parent.maxDepth;
@@ -28,32 +52,7 @@ public class Fractal : MonoBehaviour {
         childScale = parent.childScale;
         transform.parent = parent.transform;
         transform.localScale = Vector3.one * childScale;
-        transform.localPosition = direction * (0.5f + 0.5f * childScale);
-    }
-
-    private IEnumerator CreateChildren () {
-        yield return new WaitForSeconds(0.1f);
-
-        new GameObject("Fractal Child")
-            .AddComponent<Fractal>()
-            .Initialize(this, Vector3.up);
-        
-        yield return new WaitForSeconds(0.1f);
-
-        new GameObject("Fractal Child")
-            .AddComponent<Fractal>()
-            .Initialize(this, Vector3.down);
-
-        yield return new WaitForSeconds(0.1f);
-
-        new GameObject("Fractal Child")
-            .AddComponent<Fractal>()
-            .Initialize(this, Vector3.left);
-
-        yield return new WaitForSeconds(0.1f);
-
-        new GameObject("Fractal Child")
-            .AddComponent<Fractal>()
-            .Initialize(this, Vector3.right);
+        transform.localPosition = childDirections[childIndex] * (0.5f + 0.5f * childScale);
+        transform.localRotation = childOrientations[childIndex];
     }
 }
